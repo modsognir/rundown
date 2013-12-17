b1c1c96373ff2a3768c2d10a630fae16c6edc1bc
Jared Fraser <dev@jsf.io>
2013-12-17 16:19:49 +0000

    Use class interface from SentimentParser by default

--- lib/knowler/sentiment_processor.rb
+++ lib/knowler/sentiment_processor.rb
@@ -2,7 +2,7 @@ module Knowler
   class SentimentProcessor < Processor
     attr_accessor :parser
 
-    def initialize(words, parser=SentimentParser.new)
+    def initialize(words, parser=SentimentParser)
       @parser = parser
       super(words)
     end
