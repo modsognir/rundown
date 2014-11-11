1b50ad2c735f8a2c6c412f212b484fb2c0eb7a3e
Jared Fraser <dev@jsf.io>
2014-11-11 22:52:59 +0000

    Fix Hashtag processor

--- lib/rundown/processors/hashtags.rb
+++ lib/rundown/processors/hashtags.rb
@@ -2,7 +2,7 @@ module Rundown
   module Processors
     class Hashtags < Rundown::Processor
       def process
-        words.select { |word| word.start_with?('#') }
+        words.select { |word| word.start_with?('#') && word.size > 1 }
       end
     end
   end
