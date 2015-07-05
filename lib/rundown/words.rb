0c9ec724ff0731602a671945994a55ffd88d1b09
Jared Fraser <jaredfraser@Jareds-MacBook-Pro.local>
2015-07-06 09:49:16 +1000

    Use more accurate regex for splitting words

--- lib/rundown/words.rb
+++ lib/rundown/words.rb
@@ -4,7 +4,13 @@ module Rundown
     
     def initialize(text)
       @text = text
-      __setobj__(text.split(" "))
+      __setobj__(split(text))
+    end
+
+  private
+
+    def split(text)
+      text.scan(/[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/)
     end
   end
 end
